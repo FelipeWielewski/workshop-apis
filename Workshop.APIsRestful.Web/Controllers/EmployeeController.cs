03bcb326447f2bc837ebfc2e7f76ae598ed86d88
Felipe Wielewski <felipe.wielewski@neomode.com.br>
2021-10-26 08:55:18 -0300

    creating controllers

---
+++ Workshop.APIsRestful.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Workshop.APIsRestful.Business;
+using Workshop.APIsRestful.Domain.Models;
+
+namespace Workshop.APIsRestful.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmplyoeeController : ControllerBase
+    {
+        EmployeeService _service;
+
+        public EmplyoeeController(EmployeeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
